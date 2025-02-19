d2d0c444f7931465a58dbe7cb58d11f5fe97de23
eri2490 <e.147xxx@gmail.com>
2025-02-20 01:21:15 +0900

    create GhostImage

--- src/components/features/Ghost/GhostImage.tsx
+++ src/components/features/Ghost/GhostImage.tsx
@@ -1,3 +1,11 @@
+import { Container, Image, Flex } from "@chakra-ui/react";
+
 export const GhostImage = () => {
-  return <div>GhostImage</div>;
+  return (
+    <Container>
+      <Flex w="100%" alignItems="center" flexDir="column">
+        <Image src="main-image.png" alt="main image" />
+      </Flex>
+    </Container>
+  );
 };
