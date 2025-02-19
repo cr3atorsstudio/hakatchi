78980452bf64cdc082e846ca91ba241dde765972
eri2490 <e.147xxx@gmail.com>
2025-02-20 01:38:11 +0900

    create GhostOverview

--- src/components/features/Ghost/GhostOverview.tsx
+++ src/components/features/Ghost/GhostOverview.tsx
@@ -1,3 +1,29 @@
+import { Center, HStack, Text, Grid } from "@chakra-ui/react";
+
 export const GhostOverview = () => {
-  return <div>GhostOverview</div>;
+  return (
+    <Center bgColor="#FFFCEC" p={4} borderRadius="xl" mt={5}>
+      <Grid
+        gridTemplateColumns={"repeat(auto-fit, 9.25rem)"}
+        gridTemplateRows="repeat(1)"
+      >
+        {[
+          "Energy",
+          "Cleanliness",
+          "Mood",
+          "Affection",
+          "Satiety",
+          "Stress",
+        ].map((item, index) => {
+          return (
+            <HStack>
+              <Text>{item}</Text>
+              <Text>.....</Text>
+              <Text>{(index + 1) * 100}</Text>
+            </HStack>
+          );
+        })}
+      </Grid>
+    </Center>
+  );
 };
