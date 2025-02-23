690f2b1724f14d56c8734199146f0a8b570da18d
eri2490 <e.147xxx@gmail.com>
2025-02-23 11:30:52 +0900

    create GhostActionItemButton

--- src/components/features/Ghost/GhostAction.tsx
+++ src/components/features/Ghost/GhostAction.tsx
@@ -1,6 +1,7 @@
 import { HStack, Button, VStack, Image } from "@chakra-ui/react";
 import { GhostActionFeedButton } from "./GhostActionFeedButton";
 import { GhostActionCleanButton } from "./GhostActionCleanButton";
+import { GhostActionItemButton } from "./GhostActionItemButton";
 
 export const GhostAction = () => {
   return (
@@ -8,21 +9,7 @@ export const GhostAction = () => {
       <HStack w="100%" justifyContent="center" gap="0.25rem">
         <GhostActionFeedButton />
         <GhostActionCleanButton />
-        <Button
-          color="#193459"
-          w="30%"
-          h="2.875rem"
-          maxW="7.5rem"
-          pb="0.75rem"
-          pl="0.3rem"
-          letterSpacing="0.3rem"
-          backgroundColor="transparent"
-          backgroundImage={"url(/button/button2.png)"}
-          backgroundSize="contain"
-          backgroundRepeat="no-repeat"
-        >
-          ITEM
-        </Button>
+        <GhostActionItemButton />
       </HStack>
       <HStack w="100%" justifyContent="center" gap="0.25rem">
         <Button
---
+++ src/components/features/Ghost/GhostActionItemButton.tsx
@@ -0,0 +1,59 @@
+import { Button } from "@chakra-ui/react";
+
+import {
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Radio, RadioGroup } from "@/components/ui/radio";
+
+export const GhostActionItemButton = () => {
+  return (
+    <>
+      <DialogRoot>
+        <DialogTrigger asChild>
+          <Button
+            color="#193459"
+            w="30%"
+            h="2.875rem"
+            maxW="7.5rem"
+            pb="0.75rem"
+            pl="0.3rem"
+            letterSpacing="0.3rem"
+            backgroundColor="transparent"
+            backgroundImage={"url(/button/button2.png)"}
+            backgroundSize="contain"
+            backgroundRepeat="no-repeat"
+          >
+            ITEM
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Select item</DialogTitle>
+          </DialogHeader>
+          <DialogBody>
+            <RadioGroup>
+              <Radio value="apple">a</Radio>
+              <Radio value="locked1" disabled>
+                b
+              </Radio>
+              <Radio value="locked2" disabled>
+                c
+              </Radio>
+            </RadioGroup>
+          </DialogBody>
+          <DialogFooter>
+            <Button>OK</Button>
+          </DialogFooter>
+          <DialogCloseTrigger />
+        </DialogContent>
+      </DialogRoot>
+    </>
+  );
+};
