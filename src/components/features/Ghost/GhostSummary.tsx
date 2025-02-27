bdb0d95d02a24992038c82180a9d1a5a80b141c8
eri2490 <e.147xxx@gmail.com>
2025-02-27 12:57:10 +0900

    update GhostSummary

--- src/components/features/Ghost/GhostSummary.tsx
+++ src/components/features/Ghost/GhostSummary.tsx
@@ -1,3 +1,4 @@
+import { useGetGrave } from "@/hooks/useGetGrave";
 import { Container, HStack, Text } from "@chakra-ui/react";
 
 interface GhostSummaryProps {
@@ -6,15 +7,19 @@ interface GhostSummaryProps {
 }
 
 export const GhostSummary = ({ name, age }: GhostSummaryProps) => {
-  // const hoge = await useUser();
+  const { grave, error } = useGetGrave();
 
-  console.log({ name });
-
-  return (
+  return error ? (
     <Container bgColor="white">
       <HStack justifyContent="space-between" px={8} py={2}>
-        <Text>{name}</Text>
-        <Text>{age} days</Text>
+        <Text>please reload</Text>
+      </HStack>
+    </Container>
+  ) : (
+    <Container bgColor="white">
+      <HStack justifyContent="space-between" px={8} py={2}>
+        <Text>{grave ? grave.name : "--"}</Text>
+        <Text>{grave ? grave.age : "--"} days</Text>
       </HStack>
     </Container>
   );
