9c1fb93ea6ecef3fb5d1f2bec3b6a3837a90e06a
Risa Watanabe Harada <supikateriburu@gmail.com>
2025-04-20 18:02:22 +0900

    Fix #28

--- src/components/assets/Header.tsx
+++ src/components/assets/Header.tsx
@@ -1,9 +1,18 @@
-import { Box, Center, Image } from "@chakra-ui/react";
+import { Center, Image } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export const Header = () => {
+  const router = useRouter();
+
   return (
-    <Center bgColor="#5C7E5E" w="100%" minH={14}>
+    <Center
+      bgColor="#5C7E5E"
+      w="100%"
+      minH={14}
+      cursor="pointer"
+      onClick={() => router.push("/")}
+    >
       <h1>
         <Image w="100%" maxW="148px" src="logo.png" alt="HAKATCHI" />
       </h1>
